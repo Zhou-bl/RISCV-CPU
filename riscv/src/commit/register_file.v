b0770c3deaa07b9ead13de8a812c75de2369d276
Zhou_bl <zhoubinglin2002@163.com>
2022-12-17 17:22:47 +0800

    begin working on cpu.v

--- riscv/src/commit/register_file.v
+++ riscv/src/commit/register_file.v
@@ -1,1 +1,68 @@
 `include "/Users/zbl/Desktop/RISCV-CPU/riscv/src/constant.v"
+
+module register_file(
+    input wire clk,
+    input wire rst,
+    input wire rdy,
+
+    //port with dispatcher:
+    input wire alloc_signal_from_dispatcher,
+    input wire [`REG_POS_TYPE] rs1_from_dispatcher,
+    input wire [`REG_POS_TYPE] rs2_from_dispatcher,
+    input wire [`REG_POS_TYPE] rd_from_dispatcher,
+    input wire [`ROB_ID_TYPE] rob_id_for_rd_from_dispatcher,
+    output wire Q1_to_dispatcher,
+    output wire Q2_to_dispatcher,
+    output wire V1_to_dispatcher,
+    output wire V2_to_dispatcher,
+
+    //port with ROB:
+    input wire input_commit_flag,
+    input wire rollback_flag,//clear REG_Q;
+    input wire [`REG_POS_TYPE] rd_from_ROB,
+    input wire [`ROB_ID_TYPE] Q_from_ROB,
+    input wire [`DATA_TYPE] V_from_ROB
+);
+
+reg [`ROB_ID_TYPE] REG_Q [`REG_SIZE - 1 : 0];
+reg [`DATA_TYPE] REG_V[`REG_SIZE - 1 : 0];
+
+assign Q1_to_dispatcher = REG_Q[rs1_from_dispatcher];
+assign Q2_to_dispatcher = REG_Q[rs2_from_dispatcher];
+assign V1_to_dispatcher = REG_V[rs1_from_dispatcher];
+assign V2_to_dispatcher = REG_V[rs2_from_dispatcher];
+
+integer i;
+
+always @(*) begin
+    if(rst) begin
+        for(i = 0; i < `REG_SIZE; i = i + 1) begin
+            REG_Q[i] = `ZERO_ROB;
+            REG_V[i] = `ZERO_WORD;
+        end
+    end
+    else if(!rdy) begin
+    end
+    else begin
+        if(input_commit_flag) begin
+            for(i = 0; i < `REG_SIZE; i = i + 1) begin
+                REG_Q[i] = `ZERO_ROB;
+            end
+        end
+        if(alloc_signal_from_dispatcher) begin
+            if(rd_from_dispatcher != `ZERO_REG) begin
+                REG_Q[rd_from_dispatcher] = rob_id_for_rd_from_dispatcher;
+            end 
+        end
+        if(input_commit_flag) begin
+            if(rd_from_ROB != `ZERO_REG) begin
+                REG_V[rd_from_ROB] = V_from_ROB;
+            end
+            if(REG_Q[rd_from_ROB] == Q_from_ROB) begin
+                REG_Q[rd_from_ROB] = `ZERO_ROB;
+            end
+        end
+    end
+end
+
+endmodule
--- riscv/src/commit/reorder_buffer.v
+++ riscv/src/commit/reorder_buffer.v
@@ -20,6 +20,7 @@ module reorder_buffer (
     input wire [`REG_POS_TYPE] rd_from_dispatcher,
     input wire predicted_jump_result_from_dispatcher,
     input wire [`ADDR_TYPE] pc_from_dispatcher,
+    input wire [`ADDR_TYPE] rollback_pc_from_dispatcher,
     output wire [`ROB_ID_TYPE] alloced_rob_id_to_dispatcher,
 
     //port with cdb:
@@ -28,17 +29,17 @@ module reorder_buffer (
     input wire [`ROB_ID_TYPE] rob_id_from_alu,
     input wire [`DATA_TYPE] result_from_alu,
     input wire [`ADDR_TYPE] target_pc_from_alu,
-    input wire precise_jump_flag_from_alu,
+    input wire precise_jump_result_from_alu,
         //lsu:
     input wire update_signal_from_lsu,
     input wire [`ROB_ID_TYPE] rob_id_from_lsu,
-    input wire [`DATA_TYPE] result_from_lsu,
+    input wire [`DATA_TYPE] result_from_lsu,//传入load的结果;
 
     //port with lsb(for IO):
     input wire [`ROB_ID_TYPE] io_ins_rob_id_from_LSB,
 
     //port for commit:
-    output reg commit_flag,
+    output reg output_commit_flag,
         //reg:
     output reg [`REG_POS_TYPE] rd_to_reg_file,
     output reg [`ROB_ID_TYPE] Q_to_reg_file,
@@ -51,7 +52,7 @@ module reorder_buffer (
     output wire [`ROB_ID_TYPE] io_rob_id_to_lsb,
         //predictor:
     output reg enable_signal_to_predictor,
-    output reg hit_signal_to_predictor,
+    output reg precise_branch_flag,
     output reg [`ADDR_TYPE] pc_to_predictor, 
     output wire full_signal
 );
@@ -90,7 +91,7 @@ always @(posedge clk) begin
         cur_ROB_size <= 0;
         head <= 0;
         tail <= 0;
-        commit_flag <= `FALSE;
+        output_commit_flag <= `FALSE;
         enable_signal_to_predictor <= `FALSE;
         for(i = 0; i <= 15; i = i + 1) begin
             ROB_busy[i] <= `FALSE;
@@ -113,7 +114,67 @@ always @(posedge clk) begin
         //1.当head所在元素就绪时commit
         //2.当enable_signal_from_dispatcher为true时插入新的instructin
         //3.监听cdb总线对ROB中的信息进行更新.
-        
+        output_commit_flag <= `FALSE;
+        misbranch_flag <= `FALSE;
+        enable_signal_to_predictor <= `FALSE;
+        cur_ROB_size <= cur_ROB_size - commit_signal + alloc_signal_from_dispatcher;
+        //commit:
+        if(commit_signal) begin
+            //to reg file and LSB:
+            if(ROB_is_jump[head]) begin
+                enable_signal_to_predictor <= `TRUE;
+                pc_to_predictor <= ROB_pc[head];
+                precise_branch_flag <= ROB_precise_jump_result[head];
+                if(ROB_precise_jump_result[head] != ROB_predicted_jump_result[head]) begin
+                    misbranch_flag <= `TRUE;
+                    target_pc_to_if <= ROB_precise_jump_result[head] ? ROB_target_pc[head] : ROB_rollback_pc[head];
+                end
+            end
+            output_commit_flag <= `TRUE;
+            rob_id_to_lsb <= head + 1;
+            rd_to_reg_file <= ROB_rd[head];
+            Q_to_reg_file <= head + 1;
+            V_to_reg_file <= ROB_value[head];
+            ROB_busy[head] <= `FALSE;
+            ROB_is_ready[head] <= `FALSE;
+            ROB_is_IO[head] <= `FALSE;
+            ROB_is_jump[head] <= `FALSE;
+            ROB_is_store[head] <= `FALSE;
+            ROB_predicted_jump_result[head] <= `FALSE;
+            head <= next_head;
+        end
+        //alloc:
+        if(alloc_signal_from_dispatcher) begin
+            ROB_busy[tail] <= `TRUE;
+            ROB_is_ready[tail] <= `FALSE;
+            ROB_is_IO[tail] <= `FALSE;
+            ROB_value[tail] <= `ZERO_WORD;
+            ROB_precise_jump_result[tail] <= `FALSE;
+            ROB_target_pc[tail] <= `ZERO_ADDR;
+            ROB_is_jump[tail] <= is_jump_flag_from_dispatcher;
+            ROB_predicted_jump_result[tail] <= predicted_jump_result_from_dispatcher;
+            ROB_is_store[tail] <= is_store_flag_from_dispatcher;
+            ROB_pc[tail] <= pc_from_dispatcher;
+            ROB_rollback_pc[tail] <= rollback_pc_from_dispatcher;
+            ROB_rd[tail] <= rd_from_dispatcher;
+            tail <= next_tail;
+        end
+        //update:
+        if(io_ins_rob_id_from_LSB != `ZERO_ROB && ROB_busy[io_ins_rob_id_from_LSB - 1]) begin
+            ROB_is_IO[io_ins_rob_id_from_LSB - 1] <= `TRUE;
+        end
+
+        if(update_signal_from_alu && ROB_busy[rob_id_from_alu - 1]) begin
+            ROB_is_ready[rob_id_from_alu - 1] <= `TRUE;
+            ROB_value[rob_id_from_alu - 1] <= result_from_alu;
+            ROB_precise_jump_result[rob_id_from_alu - 1] <= precise_jump_result_from_alu;
+            ROB_target_pc[rob_id_from_alu - 1] <= target_pc_from_alu;
+        end
+        if(update_signal_from_lsu && ROB_busy[rob_id_from_lsu - 1]) begin
+            ROB_is_ready[rob_id_from_lsu - 1] <= `TRUE;
+            ROB_value[rob_id_from_lsu - 1] <= result_from_lsu;
+        end
+
     end
 end
 
